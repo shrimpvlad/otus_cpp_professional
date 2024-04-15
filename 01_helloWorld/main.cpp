8a35c851491679c5e1786b66aac876e262fe1357
shrimpvlad <shrimpvlad@gmail.com>
2024-04-16 01:06:11 +0300

    add tasks: ip_filter and print_ip

--- 01_helloWorld/main.cpp
+++ 01_helloWorld/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main (int, char **) {
-    std::cout << "Version: " << BUILD_NUMBER << std::endl;
+    std::cout << "Version: " << VERSION << std::endl;
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
