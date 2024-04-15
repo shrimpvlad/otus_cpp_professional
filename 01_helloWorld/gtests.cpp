bdf2cf4424689dce0f3d4c99e8916e03e3cb7591
shrimpvlad <shrimpvlad@gmail.com>
2024-04-16 02:08:47 +0300

    add tasks: ip_filter and print_ip

--- 01_helloWorld/gtests.cpp
+++ 01_helloWorld/gtests.cpp
@@ -2,6 +2,6 @@
 #include "version.h"
 
 TEST(CheckVersionNumber, correct_version) {
-    // ASSERT_GT(int(VERSION), 1);
-    ASSERT_GT(100, 1);
+    ASSERT_GT(VERSION, 1);
+    // ASSERT_GT(100, 1);
 }
