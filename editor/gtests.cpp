d7b695ff961e7e68afba375d6929ccdf402ef0ef
shrimpvlad <shrimpvlad@gmail.com>
2024-05-07 14:45:53 +0300

    add task: 05_editor

---
+++ editor/gtests.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "IShape.hpp"
+#include "EditorModel.hpp"
+#include "EditorView.hpp"
+#include "EditorController.hpp"
+
+TEST(Editor, Empty)
+{
+    ASSERT_EQ(10, 10);
+}
